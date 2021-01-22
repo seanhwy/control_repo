b74cfe2e78d6cee73433b359f05524ac57596690
root <root@topstone.localdomain>
2021-01-22 03:03:19 -0800

    added if facts kernelversion

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,15 @@
 node default {
+if ($OperatingSystem == "Linux") { 
+   $message = "This machine OS is of the type $OperatingSystem \n" 
+} else { 
+   $message = "This machine is unknown \n" 
+} 
+
+file { "/tmp/machineOperatingSystem.txt": 
+   ensure => file, 
+   content => "$message" 
+}
+
   file {'/root/README':
     ensure => file,
     content => 'zoomzoom mazda miata nd2 update from topstone Jan 4',
