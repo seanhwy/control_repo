13c3a570821f31ae92df62dce41493254abe5af9
seanhwy <51217707+seanhwy@users.noreply.github.com>
2021-01-26 22:03:35 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,29 +1,18 @@
 node default {
- if ($facts[kernelversion] == "3.10.9") { 
-   $message = "This machine OS is of the type $kernelversion \n" 
- } else { 
-   $message = "This machine is unknown wrong \n" 
-} 
-
-file { "/tmp/machineOperatingSystem.txt": 
-   ensure => file, 
-   content => "$message" 
+if ($OperatingSystem == "Linux") {
+   $message = "This machine OS is of the type $OperatingSystem \n"
+} else {
+   $message = "This machine is unknown \n"
 }
 
-file {'/root/README':
+file { "/tmp/machineOperatingSystem.txt":
+   ensure => file,
+   content => "$message"
+}
+
+  file {'/root/README':
     ensure => file,
-    content => 'zoomzoom mazda miata nd2 update from topstone Jan 210126',
+    content => 'zoomzoom mazda miata nd2 update from topstone Jan 4',
     owner => 'root',
+  }
 }
- 
-
-
-
-node /^web/ {
-  include role::app_server
-}
-node /^db/ {
-  include role::db_server
-}
-
-
