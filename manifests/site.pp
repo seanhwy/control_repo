a7b9cffd083b1f439190b8e1869399ee1cc8c180
seanhwy <51217707+seanhwy@users.noreply.github.com>
2021-01-27 01:05:55 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,4 +16,12 @@ file {'/root/README':
     owner => 'root',
 }
 
+node 'master.puppet.vm' {
+  include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
+
 }
